c252446fa6770da1fb0aa2694152a6577b5f6a1e
Josh Larson <jplhomer@gmail.com>
2019-01-26 10:35:50 -0600

    Fetch stubbed athlete data

--- pages/athlete.js
+++ pages/athlete.js
@@ -1,13 +1,24 @@
+import { fetchAthletePerformance } from "../lib/data";
+
 export default class Athlete extends React.Component {
   static async getInitialProps({ query }) {
     const { id } = query;
+    const { year } = query;
+    const performance = await fetchAthletePerformance(id, year || (new Date()).getYear());
 
     return {
-      id
+      athlete: performance.entrant,
+      scores: performance.scores
     }
   }
 
   render() {
-    return <h1>Hi, {this.props.id}</h1>;
+    const { athlete } = this.props;
+    return (
+      <div>
+        <h1>{athlete.competitorName}</h1>
+        <img src={`https://profilepicsbucket.crossfit.com/${athlete.profilePicS3key}`} alt={athlete.competitorName} />
+      </div>
+    );
   }
 }
