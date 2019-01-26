fd2c084115ca40f7f47e6f42a114eb279a72079f
Josh Larson <jplhomer@gmail.com>
2019-01-26 17:54:28 -0600

    Create workout component

--- components/score.js
+++ components/score.js
@@ -1,4 +1,5 @@
 import { getWorkoutLabelForScore, getWorkoutForScore } from "../lib/workouts";
+import Workout from "./workout";
 
 export default ({ score, athlete, meta, workouts }) => {
   const workout = getWorkoutForScore(score, workouts, athlete, meta);
@@ -7,17 +8,11 @@ export default ({ score, athlete, meta, workouts }) => {
   return (
     <div>
       <h3>{workoutLabel}: {score.scoreDisplay}</h3>
-      <img src={workout.media_preview[0].url} alt={workoutLabel} />
       <p>{score.scaled === "1" ? "Scaled" : "Rx"}</p>
       <p>{score.breakdown}</p>
       <p>Rank: {score.rank}</p>
 
-      <div dangerouslySetInnerHTML={{
-        __html: workout.athleteWorkout.steps
-      }} />
-      <div dangerouslySetInnerHTML={{
-        __html: workout.workout_summary
-      }} />
+      <Workout workout={workout} workoutLabel={workoutLabel} />
     </div>
   )
 }
---
+++ components/workout.js
@@ -0,0 +1,13 @@
+export default ({ workout, workoutLabel }) => {
+  return (
+    <div>
+      <img src={workout.media_preview[0].url} alt={workoutLabel} />
+      <div dangerouslySetInnerHTML={{
+        __html: workout.athleteWorkout.steps
+      }} />
+      <div dangerouslySetInnerHTML={{
+        __html: workout.workout_summary
+      }} />
+    </div>
+  )
+}
