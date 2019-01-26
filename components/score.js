7dabfff4d042931318dd9721d421c6a3cecb1965
Josh Larson <jplhomer@gmail.com>
2019-01-26 15:59:23 -0600

    Add supporting workout functions

--- components/score.js
+++ components/score.js
@@ -1,14 +1,20 @@
-import { getWorkoutLabelForScore } from "../lib/workouts";
+import { getWorkoutLabelForScore, getWorkoutForScore } from "../lib/workouts";
 
 export default ({ score, athlete, meta, workouts }) => {
-  const matchingWorkout = workouts.find(w => w.index === score.ordinal);
+  const workout = getWorkoutForScore(score, workouts, athlete, meta);
+  const workoutLabel = getWorkoutLabelForScore(score, athlete, meta);
 
   return (
     <div>
-      <h3>#{getWorkoutLabelForScore(score, athlete, meta)}: {score.scoreDisplay}</h3>
+      <h3>{workoutLabel}: {score.scoreDisplay}</h3>
       <p>{score.scaled === "1" ? "Scaled" : "Rx"}</p>
       <p>{score.breakdown}</p>
       <p>Rank: {score.rank}</p>
+
+      <h4>Workout Steps:</h4>
+      <div dangerouslySetInnerHTML={{
+        __html: workout.athleteWorkout.steps
+      }} />
     </div>
   )
 }
