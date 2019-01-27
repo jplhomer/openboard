3f3bf95773f815448821d862f456a15279bf8a94
Josh Larson <jplhomer@gmail.com>
2019-01-27 13:52:48 -0600

    Style FindAthlete

--- components/FindAthlete.js
+++ components/FindAthlete.js
@@ -12,22 +12,63 @@ export default class FindAthlete extends React.Component {
 
   render() {
     return (
-      <Autocomplete
-        placeholder="Enter an athlete name"
-        getItemValue={item => item.name}
-        items={this.state.athletes}
-        renderItem={(item, isHighlighted) => (
-          <div
-            key={item.id}
-            style={{ background: isHighlighted ? "lightgray" : "white" }}
-          >
-            {item.name}
-          </div>
-        )}
-        value={this.state.value}
-        onChange={this.onChange}
-        onSelect={value => this.setState({ value })}
-      />
+      <>
+        <Autocomplete
+          inputProps={{
+            className: "input",
+            placeholder: "Search for athlete"
+          }}
+          getItemValue={item => item.name}
+          items={this.state.athletes}
+          renderItem={(item, isHighlighted) => (
+            <div
+              key={item.id}
+              className="item"
+              style={{ background: isHighlighted ? "#666666" : "#888888" }}
+            >
+              {item.name}
+            </div>
+          )}
+          placeholder="Search for an athlete"
+          menuStyle={{
+            borderRadius: "3px",
+            boxShadow: "0 2px 12px rgba(0, 0, 0, 0.1)",
+            background: "transparent",
+            padding: "0 0 2px",
+            fontSize: "90%",
+            position: "fixed",
+            overflow: "auto",
+            maxHeight: "50%" // TODO: don't cheat, let it flow to the bottom
+          }}
+          value={this.state.value}
+          onChange={this.onChange}
+          onSelect={value => this.setState({ value })}
+        />
+        <style jsx>{`
+          :global(.input) {
+            padding: 0.5em;
+            background-color: #444;
+            border: 0;
+            font-size: 0.8em;
+            color: #efefef;
+            font-family: "Source Sans", sans-serif;
+          }
+
+          :global(.input:focus) {
+            outline-color: #336699;
+          }
+
+          .item {
+            padding: 0.5em;
+            background-color: #666;
+            cursor: pointer;
+          }
+
+          .menu {
+            padding-top: 0 !important;
+          }
+        `}</style>
+      </>
     );
   }
 
