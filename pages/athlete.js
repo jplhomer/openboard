de40eee790bf9b4efcba28ecd1dc992739736ea1
Josh Larson <josh.larson@voxmedia.com>
2019-10-11 07:34:21 -0500

    Update for 2020 open

--- pages/athlete.js
+++ pages/athlete.js
@@ -17,7 +17,7 @@ export default class Athlete extends React.Component {
       res.statusCode = 404;
     }
 
-    const { year = 2019 } = query;
+    const { year = 2020 } = query;
 
     const performance = await fetchAthletePerformance(id, year);
     const workouts = await fetchWorkoutsForYear(year);
